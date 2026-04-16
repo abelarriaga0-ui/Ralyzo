8efe3ccc5af62656d744e0c11eee297ffd7e060d
abelarriaga0-ui <abelarriaga0@gmail.com>
2026-04-16 12:33:51 -0500

    Update blog-menu.js

--- blog-menu.js
+++ blog-menu.js
@@ -35,6 +35,10 @@ const BLOG_POSTS = [
     href: "/blog/errores-optimizar-imagenes.html",
     title: "Errores al optimizar imágenes"
   }
+  {
+  href: "/blog/errores-al-subir-imagenes-web.html",
+  title: "Errores al subir imágenes a una web"
+}
 ];
 
 function createBlogDropdownHTML() {
